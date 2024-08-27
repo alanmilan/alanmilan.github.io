783dbe5a2c7dbd29d65188fb69c26fd67ca452de
Alan Milan <165529139+alanmilan@users.noreply.github.com>
2024-08-26 23:44:42 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const phrases = ["software Developer","dev", "fighter", "a Lover"];
+  const el = document.getElementById("words");
+
+  let sleepTime = 100;
+
+  let curPhraseIndex = 0;
+
+  const writeLoop = async () => {
+    while (true) {
+      let curWord = phrases[curPhraseIndex];
+
+      for (let i = 0; i < curWord.length; i++) {
+        el.innerText = curWord.substring(0, i + 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 10);
+
+      for (let i = curWord.length; i > 0; i--) {
+        el.innerText = curWord.substring(0, i - 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 5);
+
+      if (curPhraseIndex === phrases.length - 1) {
+        curPhraseIndex = 0;
+      } else {
+        curPhraseIndex++;
+      }
+    }
+  };
+
+  writeLoop();
+
+  document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
