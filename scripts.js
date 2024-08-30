ad81801f02d0bc7a38bf3c6461e63f1bd93619e7
Alan Milan <165529139+alanmilan@users.noreply.github.com>
2024-08-30 15:46:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,49 +1,49 @@
 function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  const phrases = ["software Developer","dev", "Python dev", "FrontEnd dev"];
-  const el = document.getElementById("words");
+const phrases = ["software Developer","dev", "Python dev", "FrontEnd dev"];
+const el = document.getElementById("words");
 
-  let sleepTime = 100;
+let sleepTime = 100;
 
-  let curPhraseIndex = 0;
+let curPhraseIndex = 0;
 
-  const writeLoop = async () => {
-    while (true) {
-      let curWord = phrases[curPhraseIndex];
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
 
-      for (let i = 0; i < curWord.length; i++) {
-        el.innerText = curWord.substring(0, i + 1);
-        await sleep(sleepTime);
-      }
-
-      await sleep(sleepTime * 10);
-
-      for (let i = curWord.length; i > 0; i--) {
-        el.innerText = curWord.substring(0, i - 1);
-        await sleep(sleepTime);
-      }
-
-      await sleep(sleepTime * 5);
-
-      if (curPhraseIndex === phrases.length - 1) {
-        curPhraseIndex = 0;
-      } else {
-        curPhraseIndex++;
-      }
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
     }
-  };
 
-  writeLoop();
+    await sleep(sleepTime * 10);
 
-  document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
 });
 
 const modal = document.getElementById("modal");
@@ -51,18 +51,18 @@ const modalImg = document.getElementById("modal-img");
 const closeModal = document.getElementsByClassName("close")[0];
 
 document.querySelectorAll('.certificado').forEach(img => {
-    img.addEventListener('click', function() {
-        modal.style.display = "flex";
-        modalImg.src = this.src;
-    });
+  img.addEventListener('click', function() {
+    modal.style.display = "flex";
+    modalImg.src = this.src;
+  });
 });
 
-closeModal.onclick = function() {
-    modal.style.display = "none";
-};
+closeModal.addEventListener('click', () => {
+  modal.style.display = "none";
+});
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
